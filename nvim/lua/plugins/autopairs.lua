e23c0085c78e49fcee2d00602f03255a0e14cc05
erikbloodshed <erik.bloodshed@hotmail.com>
2025-02-03 08:00:24 +0800

    Replace nvim-cmp with blink.cmp

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -2,33 +2,37 @@ return {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
     config = function()
-        local autopairs = require("nvim-autopairs")
-        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-        local cmp = require("cmp")
+        if pcall(require, "cmp") then
+            local autopairs = require("nvim-autopairs")
+            local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+            local cmp = require("cmp")
 
-        autopairs.setup({
-            map_cr = true,
-        })
+            autopairs.setup({
+                map_cr = true,
+            })
 
-        -- cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-        local handlers = require("nvim-autopairs.completion.handlers")
+            -- cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+            local handlers = require("nvim-autopairs.completion.handlers")
 
-        cmp.event:on(
-            "confirm_done",
-            cmp_autopairs.on_confirm_done({
-                filetypes = {
-                    -- "*" is a alias to all filetypes
-                    ["*"] = {
-                        ["("] = {
-                            kind = {
-                                cmp.lsp.CompletionItemKind.Function,
-                                cmp.lsp.CompletionItemKind.Method,
+            cmp.event:on(
+                "confirm_done",
+                cmp_autopairs.on_confirm_done({
+                    filetypes = {
+                        -- "*" is a alias to all filetypes
+                        ["*"] = {
+                            ["("] = {
+                                kind = {
+                                    cmp.lsp.CompletionItemKind.Function,
+                                    cmp.lsp.CompletionItemKind.Method,
+                                },
+                                handler = handlers["*"],
                             },
-                            handler = handlers["*"],
                         },
                     },
-                },
-            })
-        )
+                })
+            )
+        else
+            require("nvim-autopairs").setup({ map_cr = true })
+        end
     end,
 }
