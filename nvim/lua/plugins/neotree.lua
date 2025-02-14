181abe3e128b86fae57d20496f148392d3cb646c
erikbloodshed <erik.bloodshed@hotmail.com>
2025-02-14 18:51:48 +0800

    Neovim: Refactoring neotree plugins to hide cursor on enter

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -1,37 +1,5 @@
 return {
     "nvim-neo-tree/neo-tree.nvim",
-    enabled = true,
-    keys = {
-        {
-            "<leader>ef",
-            function()
-                require("neo-tree.command").execute({
-                    toggle = true,
-                    dir = vim.fn.expand("%:p:h"),
-                })
-            end,
-        },
-        {
-            "<leader>eb",
-            function()
-                require("neo-tree.command").execute({
-                    toggle = true,
-                    position = "right",
-                    source = "buffers",
-                })
-            end,
-        },
-        {
-            "<leader>cc",
-            function()
-                require("neo-tree.command").execute({
-                    toggle = true,
-                    dir = vim.fn.stdpath("config"),
-                })
-            end,
-        },
-    },
-
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons",
@@ -39,6 +7,17 @@ return {
     },
 
     config = function()
+        local hide_cursor = function()
+            local hl = vim.api.nvim_get_hl(0, { name = "Cursor", link = false })
+            vim.api.nvim_set_hl(0, "Cursor", { blend = 100, fg = hl.fg, bg = hl.bg })
+            vim.opt.guicursor:append("a:Cursor/lCursor")
+        end
+        local show_cursor = function()
+            local hl = vim.api.nvim_get_hl(0, { name = "Cursor", link = false })
+            vim.api.nvim_set_hl(0, "Cursor", { blend = 0, fg = hl.fg, bg = hl.bg })
+            vim.opt.guicursor:remove("a:Cursor/lCursor")
+        end
+
         require("neo-tree").setup({
             enable_git_status = false,
             source_selector = {
@@ -53,7 +32,6 @@ return {
             },
             close_if_last_window = true,
             popup_border_style = "rounded",
-            use_popups_for_input = true,
             filesystem = {
                 bind_to_cwd = false,
                 follow_current_file = { enabled = true },
@@ -72,6 +50,18 @@ return {
             },
             event_handlers = {
                 {
+                    event = "neo_tree_buffer_enter",
+                    handler = function()
+                        hide_cursor()
+                    end,
+                },
+                {
+                    event = "neo_tree_buffer_leave",
+                    handler = function()
+                        show_cursor()
+                    end,
+                },
+                {
                     event = "file_opened",
                     handler = function()
                         require("neo-tree.command").execute({ action = "close" })
@@ -80,12 +70,44 @@ return {
                 {
                     event = "neo_tree_popup_input_ready",
                     handler = function(args)
-                        vim.keymap.set("i", "<esc>", vim.cmd.stopinsert, { noremap = true, buffer = args.bufnr })
+                        show_cursor()
+                        vim.keymap.set("i", "<esc>", vim.cmd.stopinsert, { buffer = args.bufnr })
                     end,
                 },
             },
         })
 
-        vim.api.nvim_set_hl(0, "NeoTreeRootName", { fg = "#e0af68", italic = false, bold = true })
+        vim.api.nvim_set_hl(0, "NeoTreeRootName", { fg = "#7aa2f7", italic = false, bold = true })
     end,
+
+    keys = {
+        {
+            "<leader>ef",
+            function()
+                require("neo-tree.command").execute({
+                    toggle = true,
+                    dir = vim.fn.expand("%:p:h"),
+                })
+            end,
+        },
+        {
+            "<leader>cc",
+            function()
+                require("neo-tree.command").execute({
+                    toggle = true,
+                    dir = vim.fn.stdpath("config"),
+                })
+            end,
+        },
+        {
+            "<leader>eb",
+            function()
+                require("neo-tree.command").execute({
+                    toggle = true,
+                    position = "right",
+                    source = "buffers",
+                })
+            end,
+        },
+    },
 }
