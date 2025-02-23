05b0f66632f510cbaababcb296c61f03b362d55d
erikbloodshed <erik.bloodshed@hotmail.com>
2025-02-23 23:54:46 +0800

    modified:   nvim/lua/autocmds.lua 	modified:   nvim/lua/plugins/conform.lua 	modified:   nvim/lua/settings.lua

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -23,16 +23,17 @@ end
 vim.o.updatetime = 300
 vim.o.timeoutlen = 500
 vim.o.ttimeoutlen = 10
-vim.o.cursorline = true
-vim.o.cursorlineopt = "both"
+vim.o.swapfile = false
 vim.o.hidden = true
 vim.o.history = 100
 vim.o.number = true
 vim.o.showtabline = 0
 vim.o.splitright = true
-vim.o.swapfile = false
 vim.o.synmaxcol = 128
+vim.o.autowrite = true
 vim.wo.signcolumn = "yes"
+vim.wo.cursorlineopt = "both"
+vim.wo.cursorline = true
 
 vim.o.expandtab = true
 vim.o.shiftwidth = 4
@@ -44,3 +45,4 @@ vim.o.wrap = false
 
 vim.opt.viewoptions:append({ options = true })
 vim.opt.shortmess:append({ c = true, C = true })
+vim.opt.formatoptions:remove({ "c", "r", "o" })
