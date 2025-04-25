9697ad0bb4b87f9c7fe5d4b80fb7bc1981358da5
xenyan <xenyan.matulac@gmail.com>
2025-04-25 08:44:13 +0800

    Neovim: Refactor cpp build task.

--- nvim/lua/cpp-tools/build_task.lua
+++ nvim/lua/cpp-tools/build_task.lua
@@ -2,6 +2,7 @@ local utils = require("cpp-tools.utils")
 local RunTask = require("cpp-tools.run_task")
 local AssemblyTask = require("cpp-tools.assembly_task")
 local DataSelectorTask = require("cpp-tools.data_selector")
+local CompileTask = require("cpp-tools.compile_task")
 
 local BuildTask = {}
 BuildTask.__index = BuildTask
@@ -9,6 +10,7 @@ BuildTask.__index = BuildTask
 function BuildTask.new(config)
     local self = setmetatable({}, BuildTask)
     self.config = config
+    self.compile_task = CompileTask.new(config)
     self.run_task = RunTask.new(config)
     self.assembly_task = AssemblyTask.new(config)
     self.data_select_task = DataSelectorTask.new(config) -- Create an instance!
@@ -19,23 +21,13 @@ function BuildTask.new(config)
 end
 
 function BuildTask:compile()
-    local compiler = self.config:get("compiler")
-    local flags = utils.get_compile_flags(".compile_flags")
-    local outfile = self.config:get("output_directory") .. vim.fn.expand("%:t:r")
-    local infile = vim.api.nvim_buf_get_name(0)
-    local cmd_compile = self.config:get("compile_command")
-        or string.format("%s %s -o %s %s", compiler, flags, outfile, infile)
-
-    local diagnostics = vim.diagnostic.get(0, { severity = { vim.diagnostic.severity.ERROR } })
-
-    if vim.tbl_isempty(diagnostics) then
-        vim.cmd("!" .. cmd_compile)
-        return true
-    end
-
-    utils.goto_first_diagnostic(diagnostics)
-
-    return false
+    local args = {
+        compiler = self.config:get("compiler"),
+        flags = utils.get_compile_flags(".compile_flags"),
+        outfile = self.config:get("output_directory") .. vim.fn.expand("%:t:r"),
+        infile = vim.api.nvim_buf_get_name(0)
+    }
+    return self.compile_task:compile(args, function(cmd) vim.cmd("!" .. cmd) end)
 end
 
 function BuildTask:run()
@@ -47,8 +39,24 @@ function BuildTask:run()
     self.run_task:run()
 end
 
+function BuildTask:assemble()
+    local args = {
+        compiler = self.config:get("compiler"),
+        flags = utils.get_compile_flags(".compile_flags") .. " -S",
+        outfile = self.config:get("output_directory") .. vim.fn.expand("%:t:r") .. ".s",
+        infile = vim.api.nvim_buf_get_name(0)
+    }
+    return self.compile_task:compile(args, function(cmd) vim.fn.system(cmd) end)
+end
+
 function BuildTask:show_assembly()
-    self.assembly_task:show_assembly()
+    local hash = utils.get_buffer_hash()
+    if self.last_assembled_hash ~= hash then
+        if not self:assemble() then return end
+        self.last_assembled_hash = hash
+    end
+    local outfile = self.config:get("output_directory") .. vim.fn.expand("%:t:r") .. ".s"
+    utils.open(outfile)
 end
 
 function BuildTask:set_data_file(file)
@@ -71,4 +79,3 @@ end
 return {
     new = BuildTask.new,
 }
-
