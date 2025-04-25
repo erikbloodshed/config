e744ce181c25085ea021a0a8dcfc4533a06aea7a
erikbloodshed <erik.bloodshed@hotmail.com>
2025-04-25 21:01:41 +0800

    Neovim: fixed bugs and improve cpp build code.

---
+++ nvim/lua/cpp-tools/build.lua
@@ -0,0 +1,102 @@
+local utils = require("cpp-tools.utils")
+local ExecutionHandler = require("cpp-tools.execution_handler")
+
+local Build = {}
+Build.__index = Build
+
+function Build.new(config)
+    local self = setmetatable({}, Build)
+
+    self.config = config
+    self.execution_handler = ExecutionHandler.new(config)
+
+    self.compiler = self.config:get("compiler")
+    self.flags = utils.get_compile_flags(".compile_flags")
+    self.exe_file = self.config:get("output_directory") .. vim.fn.expand("%:t:r")
+    self.asm_file = self.exe_file .. ".s"
+    self.infile = vim.api.nvim_buf_get_name(0)
+
+    self.compile_cmd = self.config:get("compile_command") or
+        string.format("%s %s -o %s %s", self.compiler, self.flags, self.exe_file, self.infile)
+    self.assemble_cmd = self.config:get("assemble_command") or
+        string.format("%s %s -S -o %s %s", self.compiler, self.flags, self.asm_file, self.infile)
+
+    self.hash = { compile = nil, assemble = nil }
+
+    return self
+end
+
+function Build:process(key, callback)
+    local buffer_hash = utils.get_buffer_hash()
+    if self.hash[key] ~= buffer_hash then
+        local diagnostics = vim.diagnostic.get(0, { severity = { vim.diagnostic.severity.ERROR } })
+
+        if vim.tbl_isempty(diagnostics) then
+            callback()
+            self.hash[key] = buffer_hash
+            return true
+        end
+
+        utils.goto_first_diagnostic(diagnostics)
+        vim.notify("Source code compilation failed.", vim.log.levels.ERROR)
+
+        return false
+    else
+        vim.notify("Source code is already compiled.", vim.log.levels.WARN)
+    end
+
+    return true
+end
+
+function Build:compile()
+    self:process("compile", function() vim.cmd("!" .. self.compile_cmd) end)
+end
+
+function Build:run()
+    if not self:process("compile", function() vim.cmd("!" .. self.compile_cmd) end) then
+        vim.notify("Compilation failed or skipped, cannot run.", vim.log.levels.WARN)
+        return
+    end
+    self.execution_handler:run(self.exe_file)
+end
+
+-- function Build:assemble()
+--     local hash = utils.get_buffer_hash()
+--     local success = true
+--
+--     if self.last_assembled_hash ~= hash then
+--         success = utils.compile(function()
+--             vim.cmd("silent! write")
+--             vim.fn.system(self.assemble_cmd)
+--         end)
+--
+--         if success then
+--             self.last_assembled_hash = hash
+--         end
+--     end
+--
+--     return success
+-- end
+
+function Build:show_assembly()
+    if not self:process("assemble", function()
+            vim.cmd("silent! write")
+            vim.fn.system(self.assemble_cmd)
+        end) then
+        vim.notify("Compilation failed or skipped, cannot run.", vim.log.levels.WARN)
+        return
+    end
+    utils.open(self.asm_file)
+end
+
+function Build:add_data_file()
+    self.execution_handler:select_data_file() -- Call method on instance
+end
+
+function Build:remove_data_file()
+    self.execution_handler:remove_data_file() -- Call method on instance
+end
+
+return {
+    new = Build.new,
+}
--- nvim/lua/cpp-tools/init.lua
+++ nvim/lua/cpp-tools/init.lua
@@ -4,17 +4,16 @@ function M.setup(opts)
     vim.opt_local.cinkeys:remove(":")
     vim.opt_local.cindent = true
 
-    local config = require("cpp-tools.config").new() -- Create a Config instance
-    config:setup(opts) -- Use the instance's setup method
+    local config = require("cpp-tools.config").new()
+    config:setup(opts)
 
-    local task = require("cpp-tools.build_task").new(config)
+    local build = require("cpp-tools.build").new(config)
     local arg = { buffer = vim.api.nvim_get_current_buf(), noremap = true }
-
-    vim.keymap.set("n", "<leader>rc", function() task:compile() end, arg)
-    vim.keymap.set("n", "<leader>rr", function() task:run() end, arg)
-    vim.keymap.set("n", "<leader>ra", function() task:show_assembly() end, arg)
-    vim.keymap.set("n", "<leader>fa", function() task:add_data_file() end, arg)
-    vim.keymap.set("n", "<leader>fr", function() task:remove_data_file() end, arg)
+    vim.keymap.set("n", "<leader>rc", function() build:compile() end, arg)
+    vim.keymap.set("n", "<leader>rr", function() build:run() end, arg)
+    vim.keymap.set("n", "<leader>ra", function() build:show_assembly() end, arg)
+    vim.keymap.set("n", "<leader>fa", function() build:add_data_file() end, arg)
+    vim.keymap.set("n", "<leader>fr", function() build:remove_data_file() end, arg)
 end
 
 return M
