39f5bbb03d305a40a8b98e7c4c83babe8916e06d
erikbloodshed <erik.bloodshed@hotmail.com>
2025-04-27 09:42:54 +0800

    Neovim: create cpp-tools plugin

--- nvim/after/ftplugin/c.lua
+++
@@ -1,4 +0,0 @@
-require("cpp-tools").setup({
-    compiler = "gcc",
-    default_flags = "-std=c23 -O2"
-})
--- nvim/lua/cpp-tools/init.lua
+++ nvim/lua/cpp-tools/init.lua
@@ -1,19 +1,23 @@
 M = {}
 
 function M.setup(opts)
-    vim.opt_local.cinkeys:remove(":")
-    vim.opt_local.cindent = true
+    vim.api.nvim_create_autocmd("Filetype", {
+        pattern = { "c", "cpp" },
+        callback = function(args)
+            vim.opt_local.cinkeys:remove(":")
+            vim.opt_local.cindent = true
 
-    local config = require("cpp-tools.config").new()
-    config:setup(opts)
+            local config = require("cpp-tools.config").new(opts)
+            local build = require("cpp-tools.build").new(config)
+            local arg = { buffer = args.buf, noremap = true }
 
-    local build = require("cpp-tools.build").new(config)
-    local arg = { buffer = vim.api.nvim_get_current_buf(), noremap = true }
-    vim.keymap.set("n", "<leader>rc", function() build:compile() end, arg)
-    vim.keymap.set("n", "<leader>rr", function() build:run() end, arg)
-    vim.keymap.set("n", "<leader>ra", function() build:show_assembly() end, arg)
-    vim.keymap.set("n", "<leader>fa", function() build:add_data_file() end, arg)
-    vim.keymap.set("n", "<leader>fr", function() build:remove_data_file() end, arg)
+            vim.keymap.set("n", "<leader>rc", function() build:compile() end, arg)
+            vim.keymap.set("n", "<leader>rr", function() build:run() end, arg)
+            vim.keymap.set("n", "<leader>ra", function() build:show_assembly() end, arg)
+            vim.keymap.set("n", "<leader>fa", function() build:add_data_file() end, arg)
+            vim.keymap.set("n", "<leader>fr", function() build:remove_data_file() end, arg)
+        end,
+    })
 end
 
 return M
