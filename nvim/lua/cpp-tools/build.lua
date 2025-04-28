e97ac59127b1f95cbbcc028fddcae6c7288753bf
xenyan <xenyan.matulac@gmail.com>
2025-04-28 15:07:57 +0800

    Nvim: c++ build script improvements using closures

--- nvim/lua/cpp-tools/build.lua
+++ nvim/lua/cpp-tools/build.lua
@@ -4,168 +4,174 @@ local Build = {}
 Build.__index = Build
 
 function Build.new(config, ft)
-    local self          = setmetatable({}, Build)
+    local self = setmetatable({}, Build)
 
-    local config_ft     = config:get(ft)
-    local config_dir    = config:get("dir")
+    local config_ft = config:get(ft)
+    local config_dir = config:get("dir")
 
-    self.handler        = require("cpp-tools.handler").new()
+    local handler = require("cpp-tools.handler").new()
 
-    self.compiler       = config_ft.compiler
-    self.compile_opts   = config_ft.compile_opts
-    self.fallback_flags = config_ft.fallback_flags
-    self.compile_cmd    = config_ft.compile_cmd
-    self.assemble_cmd   = config_ft.assemble_cmd
-    self.output_dir     = config_dir.output_directory
-    self.data_dir       = config_dir.data_dir_name
+    -- Capture all the static stuff into locals
+    local compiler = config_ft.compiler
+    local compile_opts = config_ft.compile_opts
+    local fallback_flags = config_ft.fallback_flags
+    local compile_cmd = config_ft.compile_cmd
+    local assemble_cmd = config_ft.assemble_cmd
+    local output_dir = config_dir.output_directory
+    local data_dir = config_dir.data_dir_name
 
-    self.options_file   = utils.get_options_file(self.compile_opts)
-    self.flags          = self.options_file or self.fallback_flags
-    self.exe_file       = self.output_dir .. vim.fn.expand("%:t:r")
-    self.asm_file       = self.exe_file .. ".s"
-    self.infile         = vim.api.nvim_buf_get_name(0)
-    self.data_path      = utils.get_data_path(self.data_dir)
-    self.data_file      = nil
-    self.hash           = { compile = nil, assemble = nil }
+    local options_file = utils.get_options_file(compile_opts)
+    local flags = options_file or fallback_flags
+    local infile = vim.api.nvim_buf_get_name(0)
+    local exe_file = output_dir .. vim.fn.expand("%:t:r")
+    local asm_file = exe_file .. ".s"
+    local data_path = utils.get_data_path(data_dir)
 
-    self.ft             = ft
-    self.cmp_command    = self:get_compile_command()
-    self.asm_command    = self:get_assemble_command()
-    return self
-end
+    local hash = { compile = nil, assemble = nil }
+    local data_file = nil
 
-function Build:process(key, callback)
-    if vim.bo.modified then
-        vim.cmd("silent! write")
+    -- These are closures now
+    local function get_compile_command()
+        return compile_cmd or string.format(
+            "%s %s -o %s %s",
+            compiler,
+            flags,
+            exe_file,
+            infile
+        )
     end
-    local buffer_hash = utils.get_buffer_hash()
-    if self.hash[key] ~= buffer_hash then
-        local diagnostics = vim.diagnostic.get(0, { severity = { vim.diagnostic.severity.ERROR } })
 
-        if vim.tbl_isempty(diagnostics) then
-            callback()
-            self.hash[key] = buffer_hash
-            return true
+    local function get_assemble_command()
+        return assemble_cmd or string.format(
+            "%s %s -S -o %s %s",
+            compiler,
+            flags,
+            asm_file,
+            infile
+        )
+    end
+
+    -- Core methods
+    function self:process(key, callback)
+        if vim.bo.modified then
+            vim.cmd("silent! write")
+        end
+        local buffer_hash = utils.get_buffer_hash()
+        if hash[key] ~= buffer_hash then
+            local diagnostics = vim.diagnostic.get(0, { severity = { vim.diagnostic.severity.ERROR } })
+
+            if vim.tbl_isempty(diagnostics) then
+                callback()
+                hash[key] = buffer_hash
+                return true
+            end
+
+            utils.goto_first_diagnostic(diagnostics)
+            vim.notify("Source code compilation failed.", vim.log.levels.ERROR)
+
+            return false
+        else
+            vim.notify("Source code is already compiled.", vim.log.levels.WARN)
         end
 
-        utils.goto_first_diagnostic(diagnostics)
-        vim.notify("Source code compilation failed.", vim.log.levels.ERROR)
-
-        return false
-    else
-        vim.notify("Source code is already compiled.", vim.log.levels.WARN)
+        return true
     end
 
-    return true
-end
-
-function Build:compile()
-    if self:process("compile", function() vim.fn.system(self.cmp_command) end) then
-        vim.notify("Compiled successfully.", vim.log.levels.INFO)
-    end
-end
-
-function Build:run()
-    if not self:process("compile", function() vim.fn.system(self.cmp_command) end) then
-        vim.notify("Compilation failed or skipped, cannot run.", vim.log.levels.WARN)
-        return
-    end
-    self.handler:run(self.exe_file)
-end
-
-function Build:show_assembly()
-    vim.cmd("silent! write")
-    if not self:process("assemble", function()
-            vim.fn.system(self.asm_command)
+    function self:compile()
+        if self:process("compile", function()
+            vim.fn.system(get_compile_command())
         end) then
-        vim.notify("Compilation failed or skipped, cannot run.", vim.log.levels.WARN)
-        return
-    end
-    utils.open(string.format(" %s ", self.asm_file), utils.read_file(self.asm_file), "asm")
-end
-
-function Build:add_data_file()
-    if not self.data_path then return end
-    local files = utils.scan_dir(self.data_path)
-    if vim.tbl_isempty(files) then
-        vim.notify("No files found in data directory: " .. self.data_path, vim.log.levels.WARN)
-        return
-    end
-
-    local prompt = 'Current: ' .. (self.data_file or 'None') .. '):'
-    vim.ui.select(files, {
-        prompt = prompt,
-        format_item = function(item)
-            return vim.fn.fnamemodify(item, ':t')
-        end,
-    }, function(choice)
-        if choice then
-            self.data_file = choice
-            self.handler:set_data_file(self.data_file)
-            vim.notify("Data file set to: " .. vim.fn.fnamemodify(choice, ':t'), vim.log.levels.INFO)
-        end
-    end)
-end
-
-function Build:remove_data_file()
-    if self.data_file == nil then
-        vim.notify("No data file is currently set.", vim.log.levels.WARN)
-        return
-    end
-
-    vim.ui.select({ "Yes", "No" }, {
-        prompt = "Remove data file (" .. vim.fn.fnamemodify(self.data_file, ':t') .. ")?",
-    }, function(choice)
-        if choice == "Yes" then
-            self.data_file = nil
-            self.handler:set_data_file(self.data_file)
-            vim.notify("Data file removed.", vim.log.levels.INFO)
-        end
-    end)
-end
-
-function Build:get_build_info()
-    local lines = {
-        "Filetype         : " .. self.ft,
-        "Compiler         : " .. self.compiler,
-        "Compile Flags    : " .. self.flags,
-        "Source           : " .. self.infile,
-        "Output Directory : " .. self.output_dir,
-        "Data Directory   : " .. (self.data_path or ""),
-        "Data File In Use : " .. (self.data_file or ""),
-        "Date Modified    : " .. utils.get_modified_time(self.infile),
-        "Date Created     : " .. utils.get_creation_time(self.infile)
-    }
-
-    local buf = utils.open(" Compile Info ", lines, "text")
-    for i, line in ipairs(lines) do
-        local col = line:find(":")
-        if col then
-            vim.api.nvim_buf_add_highlight(buf, -1, "Keyword", i - 1, 0, col - 1)
+            vim.notify("Compiled successfully.", vim.log.levels.INFO)
         end
     end
-end
 
-function Build:get_compile_command()
-    return self.compile_cmd or string.format(
-        "%s %s -o %s %s",
-        self.compiler,
-        self.flags,
-        self.exe_file,
-        self.infile
-    )
-end
+    function self:run()
+        if not self:process("compile", function()
+            vim.fn.system(get_compile_command())
+        end) then
+            vim.notify("Compilation failed or skipped, cannot run.", vim.log.levels.WARN)
+            return
+        end
+        handler:run(exe_file)
+    end
 
-function Build:get_assemble_command()
-    return self.assemble_cmd or string.format(
-        "%s %s -S -o %s %s",
-        self.compiler,
-        self.flags,
-        self.asm_file,
-        self.infile
-    )
+    function self:show_assembly()
+        vim.cmd("silent! write")
+        if not self:process("assemble", function()
+            vim.fn.system(get_assemble_command())
+        end) then
+            vim.notify("Compilation failed or skipped, cannot run.", vim.log.levels.WARN)
+            return
+        end
+        utils.open(string.format(" %s ", asm_file), utils.read_file(asm_file), "asm")
+    end
+
+    function self:add_data_file()
+        if not data_path then return end
+        local files = utils.scan_dir(data_path)
+        if vim.tbl_isempty(files) then
+            vim.notify("No files found in data directory: " .. data_path, vim.log.levels.WARN)
+            return
+        end
+
+        local prompt = 'Current: ' .. (data_file or 'None') .. '):'
+        vim.ui.select(files, {
+            prompt = prompt,
+            format_item = function(item)
+                return vim.fn.fnamemodify(item, ':t')
+            end,
+        }, function(choice)
+            if choice then
+                data_file = choice
+                handler:set_data_file(data_file)
+                vim.notify("Data file set to: " .. vim.fn.fnamemodify(choice, ':t'), vim.log.levels.INFO)
+            end
+        end)
+    end
+
+    function self:remove_data_file()
+        if data_file == nil then
+            vim.notify("No data file is currently set.", vim.log.levels.WARN)
+            return
+        end
+
+        vim.ui.select({ "Yes", "No" }, {
+            prompt = "Remove data file (" .. vim.fn.fnamemodify(data_file, ':t') .. ")?",
+        }, function(choice)
+            if choice == "Yes" then
+                data_file = nil
+                handler:set_data_file(nil)
+                vim.notify("Data file removed.", vim.log.levels.INFO)
+            end
+        end)
+    end
+
+    function self:get_build_info()
+        local lines = {
+            "Filetype         : " .. ft,
+            "Compiler         : " .. compiler,
+            "Compile Flags    : " .. flags,
+            "Source           : " .. infile,
+            "Output Directory : " .. output_dir,
+            "Data Directory   : " .. (data_path or ""),
+            "Data File In Use : " .. (data_file or ""),
+            "Date Modified    : " .. utils.get_modified_time(infile),
+            "Date Created     : " .. utils.get_creation_time(infile)
+        }
+
+        local buf = utils.open(" Compile Info ", lines, "text")
+        for i, line in ipairs(lines) do
+            local col = line:find(":")
+            if col then
+                vim.api.nvim_buf_add_highlight(buf, -1, "Keyword", i - 1, 0, col - 1)
+            end
+        end
+    end
+
+    return self
 end
 
 return {
     new = Build.new,
 }
+
